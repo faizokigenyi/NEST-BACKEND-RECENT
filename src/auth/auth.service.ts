93fee07dac6ecfcd6d0b625f8bc9ff632ca93e30
Faizo Kigenyi <faizokigenyi@gmail.com>
2023-12-05 03:42:20 -0600

    Tuesday Dec 2023

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
 
 @Injectable({})
 export class AuthService {
+
   signup() {
     return { msg: 'i HAVE SIGNED UP' };
   }
